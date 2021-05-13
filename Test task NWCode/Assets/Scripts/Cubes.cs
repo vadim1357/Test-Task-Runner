d53d65af0b16ea429bf15f357989dc2e453923d7
vadim1357 <korobov_141198@mail.ru>
2021-05-13 11:30:05 +0300

    Vadim

--- Test task NWCode/Assets/Scripts/Cubes.cs
+++ Test task NWCode/Assets/Scripts/Cubes.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Cubes : MonoBehaviour
 {
    [SerializeField] private Rigidbody rb;
+    public Text test;
    
 
  
